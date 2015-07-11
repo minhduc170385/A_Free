f96b41a6224fed6265e151287fd2a4d78639a6f9
Minh Duc <minhduc170385@yahoo.com>
2015-07-11 09:03:04 -0700

    Initial commit

---
+++ app/src/main/java/com/awsdevelop/ducbm/awsexamdevelop/codeLearnChapter.java
@@ -0,0 +1,9 @@
+package com.awsdevelop.ducbm.awsexamdevelop;
+
+/**
+ * Created by hermes on 2/12/15.
+ */
+public class codeLearnChapter {
+    String chapterName;
+    String chapterDescription;
+}
